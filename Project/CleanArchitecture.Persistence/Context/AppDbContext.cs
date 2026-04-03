9d879bfb5274a6e62ccb13469e9455a5cef513a8
Gabriel Trindade <contato.trindadegabriel@gmail.com>
2026-04-03 00:44:44 -0300

    Add EF Core setup and project references to Persistence
    - Added project references between Application, Domain, and Persistence layers.
    - Added Microsoft.EntityFrameworkCore.Sqlite package to Persistence.
    - Created AppDbContext with Users DbSet for EF Core.
    - Added folder structure for repositories in Persistence.

---
+++ Project/CleanArchitecture.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Persistence.Context
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
