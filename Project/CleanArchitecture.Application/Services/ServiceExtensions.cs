8d8af61395ee3e975541ce568e9ddcb592432198
Gabriel Trindade <contato.trindadegabriel@gmail.com>
2026-04-03 17:00:26 -0300

    Add ServiceExtensions for DI setup; update csproj folders
    Introduced ServiceExtensions with ConfigureApplicationApp to register AutoMapper, MediatR, and FluentValidation services using the current assembly. Also removed the Services\ folder reference from the project file.

---
+++ Project/CleanArchitecture.Application/Services/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Application.Services
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureApplicationApp(this IServiceCollection services)
+        {
+            services.AddAutoMapper(cfg => { }, Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
